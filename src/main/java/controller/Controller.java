40daa060cd75158fc19e873042dbd86e14954ffc
Thiago Barbosa Ribeiro <thiago.b.ribeiro@hotmail.com>
2023-02-15 10:54:53 -0300

    Criação do método para inserção de novo usuario no BD.

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -7,12 +7,15 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import model.DAO;
+import model.JavaBeans;
 
-@WebServlet(urlPatterns = {"/Controller", "/main"})
+@WebServlet(urlPatterns = {"/Controller", "/main", "/inserirNovoUsuario"})
 public class Controller extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	DAO dao = new DAO();
+	
+	JavaBeans usuario = new JavaBeans();
 	
     public Controller() {
         super();        
@@ -24,11 +27,32 @@ public class Controller extends HttpServlet {
 		
 		if (action.equals("/main")){
 			listarUsuarios(request, response);
+		} else if(action.equals("/inserirNovoUsuario")) {
+			inserirNovoUsuario(request, response);
+		} else {
+			response.sendRedirect("index.html");
 		}
 	}
 	
 	protected void listarUsuarios(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		response.sendRedirect("usuarios.jsp");
+	}
+	
+	protected void inserirNovoUsuario(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		String matricula = request.getParameter("matricula");
+		System.out.println(matricula);
+		
+		usuario.setMatricula(request.getParameter("matricula"));
+		usuario.setNome(request.getParameter("nome"));
+		usuario.setEmail(request.getParameter("email"));
+		usuario.setTelefone(Long.parseLong(request.getParameter("telefone")));
+		usuario.setGenero(request.getParameter("genero"));
+		usuario.setCategoria(Byte.parseByte(request.getParameter("categoria")));
+		
+		dao.inserirNovoUsuario(usuario);
+		
+		response.sendRedirect("main");
 	}
 
 }
--- src/main/java/model/DAO.java
+++ src/main/java/model/DAO.java
@@ -2,6 +2,7 @@ package model;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 
 public class DAO {
 	private String driver   = "com.mysql.cj.jdbc.Driver";
@@ -26,6 +27,29 @@ public class DAO {
 			Connection con = conectar();	
 			System.out.println(con);
 			con.close();
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}
+	
+	public void inserirNovoUsuario(JavaBeans usuario) {
+		String create = "INSERT INTO tbUsuarios (matricula, nome, email, telefone, genero, categoria) VALUES (?,?,?,?,?,?)";
+		try {
+			Connection con = conectar();
+			
+			PreparedStatement pst = con.prepareStatement(create);
+			
+			pst.setString(1, usuario.getMatricula());
+			pst.setString(2, usuario.getNome());
+			pst.setString(3, usuario.getEmail());
+			pst.setLong(4, usuario.getTelefone());
+			pst.setString(5, usuario.getGenero());
+			pst.setByte(6, usuario.getCategoria());
+			
+			pst.executeUpdate();
+			
+			con.close();
+			
 		} catch (Exception e) {
 			System.out.println(e);
 		}
